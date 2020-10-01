dd4bbf9e899eab0e46e2f4910b8cd96f852d7f47
codewithSUKU <72142656+codewithSUKU@users.noreply.github.com>
2020-10-01 12:10:44 +0530

    first commit

---
+++ jarvis.py
@@ -0,0 +1,85 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice',voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("good Morning!")
+    
+    elif hour>=12 and hour<18:
+        speak("Good afternoon!")
+
+    else:
+        speak("Good evening!")
+
+    speak("hi sukanta sir. please tell me how can i help you")
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+        
+    
+
+    except Exception as e:
+        # print(e)
+
+        print("say that again please....")
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching wikipedia...')
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query, sentances=2)
+            speak("According to wikipedia")
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'play music' in query:
+            music_dir = 'D:\my music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir,songs[0]))
+        elif'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"sukanta sir, the time is {strTime}")
+          
+        
+  
+        
+
+
+    
